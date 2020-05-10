1f40dc471ec8af37292a4d6bda70b6860f1e4977
profxj <xavier@ucolick.org>
2020-05-10 10:18:05 -0700

    notebook

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ setup_keywords['zip_safe'] = False
 #setup_keywords['use_2to3'] = True
 setup_keywords['packages'] = find_packages()
 #setup_keywords['package_dir'] = {'':'py'}
-#setup_keywords['cmdclass'] = {'version': DesiVersion, 'test': DesiTest, 'sdist': DistutilsSdist}
-#etup_keywords['test_suite']='{name}.tests.{name}_test_suite.{name}_test_suite'.format(**setup_keywords)
 setup_keywords['setup_requires']=['pytest-runner']
 setup_keywords['tests_require']=['pytest']
 
